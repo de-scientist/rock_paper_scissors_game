2423219d1b1985bb6c20a6e98226b4972562c1e3
de-scientist <gitaumark502@gmail.com>
2025-10-06 20:23:46 +0300

    add DOM elements using id's from the index file

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// DOM elements
+const playerChoice = document.getElementById("player-choice");
+const computerChoice = document.getElementById("computer-choice");
+const resultText = document.getElementById("result-text");
+const buttons = document.getElementById(".controls button");
+
