5ae70fa46d8b516a692a7cf4c3f48513dfe5d0cd
de-scientist <gitaumark502@gmail.com>
2025-10-06 20:39:47 +0300

    add an object choices

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ const playerChoice = document.getElementById("player-choice");
 const computerChoice = document.getElementById("computer-choice");
 const resultText = document.getElementById("result-text");
 const buttons = document.getElementById(".controls button");
+const playerScoreText = document.getElementById("player-score");
+const computerScoreText = document.getElementById("computer-score");
 
 // Initialize scores
 let playerScore = 0;
 let computerScore = 0;
+
+// Add choices
+const choices = {
+    rock: "✊",
+    paper: "✋",
+    scissors: "✌️"
+}
