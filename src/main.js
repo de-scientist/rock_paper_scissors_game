85d9f68ecb82a6bf076123b92eb7aec46f486bbd
de-scientist <gitaumark502@gmail.com>
2025-10-06 20:26:26 +0300

    initialize scores

--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ const computerChoice = document.getElementById("computer-choice");
 const resultText = document.getElementById("result-text");
 const buttons = document.getElementById(".controls button");
 
+// Initialize scores
+let playerScore = 0;
+let computerScore = 0;
